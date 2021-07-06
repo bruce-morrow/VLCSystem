00b5f2ffe19356c5fac0509cc07527b22c8abf59
brucemorrow <bruce.morrow@hotmail.com>
2021-07-06 13:35:21 -0300

    Adding the printBuffer and debug modes.

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,10 +1,14 @@
 #define FRAME_SIZE  16
 #define LED_PIN 13
+#define DEBUG_ON true
+#define FREQUENCY 1
 
 void byte2bits(byte b, int* bits);
 void sendFrame(byte* frame);
+void printBuffer(byte* b, int len);
 
 byte frameBuffer[FRAME_SIZE]; // 1 frame start byte - 13 data bytes - 1 parity byte - 1 frame end byte
+const float bitTime = 1.0 / (2.0 * FREQUENCY); 
 
 void setup() {
   Serial.begin(9600);
@@ -12,8 +16,12 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    Serial.readBytes(frameBuffer, 16);
+  if (Serial.available() == FRAME_SIZE + 1) {
+    Serial.readBytes(frameBuffer, FRAME_SIZE);
+    Serial.read();
+    if (DEBUG_ON) {
+      printBuffer(frameBuffer, FRAME_SIZE);
+    }
     sendFrame(frameBuffer);
   }
 }
@@ -32,6 +40,16 @@ void sendFrame(byte* frame) {
       int b = bits[j];
       if (b == 0) digitalWrite(LED_PIN, LOW);
       else digitalWrite(LED_PIN, HIGH);
+      delay(1000 * bitTime);
     }
   }
+  if (DEBUG_ON) {
+    Serial.println("End of trasmission");
+  }
+}
+
+void printBuffer(byte* b, int len) {
+  Serial.print("Buffer data: ");
+  Serial.write(b, len);
+  Serial.println();
 }
